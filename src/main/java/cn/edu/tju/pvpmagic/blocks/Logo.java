bb93ca64cabaca466224ec0a09660cf99bae86bd
Akir_ <96360624+AkirTech@users.noreply.github.com>
2025-12-11 16:29:38 +0800

    art: added logo texture

--- src/main/java/cn/edu/tju/pvpmagic/blocks/Logo.java
+++ src/main/java/cn/edu/tju/pvpmagic/blocks/Logo.java
@@ -2,17 +2,20 @@ package cn.edu.tju.pvpmagic.blocks;
 
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Tool;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.Tags;
 
 public class Logo extends Block{
     public Logo() {
         super(Properties.of().sound(SoundType.WOOD)
                 .strength(2.0f, 3.0f)
-                .mapColor(MapColor.WOOD));
+                .mapColor(MapColor.WOOD)
+                .requiresCorrectToolForDrops());
     }
 
 }
