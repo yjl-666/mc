947256728b0f870d8ad7f7993e31cb6e1af9a21b
yjl-666 <18353266503@163.com>
2025-12-13 20:08:48 +0800

    feature:magic_dust(but can't operation)

--- src/main/java/cn/edu/tju/pvpmagic/items/ModItems.java
+++ src/main/java/cn/edu/tju/pvpmagic/items/ModItems.java
@@ -10,6 +10,12 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Pvpmagic.MODID);
     public static final DeferredItem<Item> LOGO = ITEMS.register("logo",
             () -> new Logo(new Item.Properties()));
+    public static final DeferredItem<Item> MAGIC_DUST = ITEMS.register("magic_dust",
+            () -> new Item(
+                    new Item.Properties()
+                            .stacksTo(64)
+            )
+    );
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
---
+++ src/main/java/cn/edu/tju/pvpmagic/items/magic_dust.java
@@ -0,0 +1,41 @@
+package cn.edu.tju.pvpmagic.items;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
+public class magic_dust extends Item {
+    public magic_dust(Properties properties) {
+        super(properties);
+    }
+    @Override
+    public InteractionResultHolder<ItemStack>use(Level level,Player player,InteractionHand hand)
+    {
+        ItemStack itemStack=player.getItemInHand(hand);
+        if(!level.isClientSide())
+        {
+            ServerPlayer serverPlayer=(ServerPlayer)player;
+            BlockPos blockPos=serverPlayer.blockPosition();
+            int x=blockPos.getX();
+            int z=blockPos.getZ();
+            int y=255;
+            for(;;y--) {
+                BlockPos pos = new BlockPos(x, y, z);
+                if (!level.isEmptyBlock(pos)) {
+                    break;
+                }
+            }
+            serverPlayer.teleportTo(x+0.5,y+1,z+0.5);
+            itemStack.shrink(1);
+            player.getCooldowns().addCooldown(this,1);
+            return InteractionResultHolder.success(itemStack);
+        }else{
+            return InteractionResultHolder.consume(itemStack);
+        }
+
+    }
+}
